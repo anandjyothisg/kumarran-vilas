1a9da51631500a2b85ddc5fa64f3da7accc5e3a8
anandjyothisg <anandjyothis57@gmail.com>
2025-06-01 20:36:35 +0530

    Update AIChat.jsx

--- client/src/components/AIChat.jsx
+++ client/src/components/AIChat.jsx
@@ -45,7 +45,7 @@ const AIAssistant = () => {
     setIsTyping(true);
 
     try {
-      const response = await fetch('http://localhost:5000/api/chat', {
+      const response = await fetch("https://kumarran-vilas.onrender.com/chat", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
